72e71a060a0307e45df91bfd439ab1ad98d2908d
RobertWhip <il.cint@mail.ru>
2019-05-12 17:42:39 +0300

    Initial commit

---
+++ Assets/Scripts/AudioTiming.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioElement {
+		public AudioClip audio;
+		public float timing;
+	}
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioTiming : MonoBehaviour {
+	public AudioElement[] audioElems;
+	private AudioSource audioSource;
+
+    void Start()
+    {
+		audioSource = GetComponent<AudioSource>();
+		StartCoroutine("PlayAudios");
+	}
+	
+	IEnumerator PlayAudios() {
+		yield return null;
+
+		for (int i = 0; i < audioElems.Length; i++) {
+			audioSource.clip = audioElems[i].audio;
+			audioSource.Play();
+			while (audioSource.isPlaying) {
+				yield return null;
+			}
+			yield return new WaitForSeconds(audioElems[i].timing);
+		}
+	}
+}
